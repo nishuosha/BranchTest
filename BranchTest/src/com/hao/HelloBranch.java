a5bcc9e9a4eeb87ae0ee368bd22bd46aabf0b93a
hao <blessyoukilled@gmail.com>
2017-08-11 16:22:06 +0800

    local add and commit

--- BranchTest/src/com/hao/HelloBranch.java
+++ BranchTest/src/com/hao/HelloBranch.java
@@ -5,5 +5,6 @@ public class HelloBranch {
 	public static void main(String[] args) {
 		System.out.println("first master commit");
 		System.out.println("remote xiugaimouxiedongxierbushitianjia remote update");
+		System.out.println("local add some");
 	}
 }
