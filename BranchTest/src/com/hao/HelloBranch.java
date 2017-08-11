e497afd9f3a1a6a6699b62134a6f7931670a1b08
hao <blessyoukilled@gmail.com>
2017-08-11 16:08:44 +0800

    update charset

--- BranchTest/src/com/hao/HelloBranch.java
+++ BranchTest/src/com/hao/HelloBranch.java
@@ -3,7 +3,7 @@ package com.hao;
 public class HelloBranch {
 
 	public static void main(String[] args) {
-		System.out.println("µÚÒ»´Îmaster Ìá½»");
-		System.out.println("remote 修改");
+		System.out.println("first master commit");
+		System.out.println("remote update");
 	}
 }
