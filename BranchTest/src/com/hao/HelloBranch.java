d91cbe1ea30d09a45f3515923d95e7009286bb3a
张浩 <blessyoukilled@gmail.com>
2017-08-11 15:56:27 +0800

    Update HelloBranch.java

--- BranchTest/src/com/hao/HelloBranch.java
+++ BranchTest/src/com/hao/HelloBranch.java
@@ -3,9 +3,5 @@ package com.hao;
 public class HelloBranch {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Branch");
-		System.out.println("this is master branch");
-		System.out.println("this is master branch 3");
-		System.out.println("branch 6");
 	}
 }
